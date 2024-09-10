a4a3f9810c002fc75ffc4dfd12d247cca3cc2278
Beio0899 <goncalvesfragosoezequiel@gmail.com>
2024-09-09 21:05:27 -0300

    Add files via upload

---
+++ Calcular_IMC/src/main/java/com/mycompany/calcular_imc/Calcular_IMC.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcular_imc;
+
+import com.mycompany.calcular_imc.Tela.Tela;
+
+/**
+ *
+ * @author User
+ */
+public class Calcular_IMC {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tela().setVisible(true);
+            }
+        });
+    }
+}
